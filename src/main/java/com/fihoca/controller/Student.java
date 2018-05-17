6efe0109b7e956002a7fd7f26aca40d6ae42fff9
Daniel Sánchez <danisc3091@outlook.es>
2018-05-17 16:12:49 +0200

    Se han añadido logs (no me deja crear el .war con el log4j en el pom)

--- src/main/java/com/fihoca/controller/Student.java
+++ src/main/java/com/fihoca/controller/Student.java
@@ -4,6 +4,11 @@ import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 //import javax.portlet.PortletPreferences;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+//import org.apache.log4j.Logger;
+
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.util.ParamUtil;
@@ -15,7 +20,12 @@ import com.liferay.util.bridges.mvc.MVCPortlet;
  */
 public class Student extends MVCPortlet {
 	
+	//private static Logger _log = Logger.getLogger(Student.class);
+	private static Log _log = LogFactoryUtil.getLog(Student.class);
+	
 	public void addStudent(ActionRequest actionRequest, ActionResponse actionResponse){
+		
+		_log.info("Estamos en el método addStudent");
 		
 		//PortletPreferences prefs = actionRequest.getPreferences();
 		
@@ -26,12 +36,15 @@ public class Student extends MVCPortlet {
 
 		if (name.equals("") || surname.equals("") || age.equals("")) {
 			SessionErrors.add(actionRequest, "error");
+			_log.error("Error!! Hay algún campo vacío");
+			
 		} else {
 			
 			String texto = "El alumno " + name + " " + surname + " tiene " + age + " años";
 			actionResponse.setRenderParameter("actions", texto);
 			
 			SessionMessages.add(actionRequest, "success");
+			_log.fatal("Todo bien a tope!!");
 		}
 
 		
